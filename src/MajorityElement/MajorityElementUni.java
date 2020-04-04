2eca7697c0c80f86c3deee5cf63ab1e2fc9b8c6a
unini <unini056378@gmail.com>
2020-04-03 23:38:05 -0700

    solve MajorityElementUni using HashMap

--- src/MajorityElement/MajorityElementUni.java
+++ src/MajorityElement/MajorityElementUni.java
@@ -1,4 +1,30 @@
 package MajorityElement;
 
+import java.util.HashMap;
+
 public class MajorityElementUni {
+  public static void main(String[] args) {
+
+    MajorityElementUni ME = new MajorityElementUni();
+    int[] nums = { 3, 3, 4 };
+
+    System.out.println(ME.majorityElement(nums));
+  }
+
+  public int majorityElement(int[] nums) {
+    HashMap newMap = new HashMap();
+    int resultNumber = nums[0];
+
+    for (int i = 0; i < nums.length; ++i) {
+      if (newMap.containsKey(nums[i])) {
+        int value = Integer.parseInt(String.valueOf(newMap.get(nums[i]))) + 1;
+        newMap.put(nums[i], value);
+      } else {
+        newMap.put(nums[i], 1);
+      }
+      if (Integer.parseInt(String.valueOf(newMap.get(nums[i]))) > Integer.parseInt(String.valueOf(newMap.get(resultNumber)))) resultNumber = nums[i];
+    }
+
+    return resultNumber;
+  }
 }
