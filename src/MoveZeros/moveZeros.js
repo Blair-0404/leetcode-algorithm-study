b5bb37f7f8e0ea87bffd2ed60587204778c2b032
Rachel4858 <rachel458458@gmail.com>
2020-04-08 01:42:44 +0900

    Coding LeetCode MoveZeros.js

---
+++ src/MoveZeros/moveZeros.js
@@ -0,0 +1,10 @@
+function moveZeroes(nums) {
+  var idx = 0;
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[idx] = nums[i];
+      nums[i] = idx === i ? nums[i] : 0;
+      idx++;
+    }
+  }
+}
